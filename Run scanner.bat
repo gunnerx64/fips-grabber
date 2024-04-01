56ee33c772edb351f8ec92425e75d7e0ea8a1ee1
Sergey S <sviridov@vva.ru>
2024-04-01 15:46:32 +0100

    доработана инструкция по запуску

--- Run scanner.bat
+++ Run scanner.bat
@@ -3,18 +3,18 @@ set venv=venv
 
 if exist %venv%\ (
   echo venv %venv% exists
-  echo opening %venv% virtual environment...
+  echo opening %venv% virtual environment..
   call %CD%\%venv%\Scripts\activate.bat
 ) else (
-  echo creating %venv% virtual environment...
+  echo creating %venv% virtual environment..
   python -m venv %venv%
-  echo opening %venv% virtual environment...
+  echo opening %venv% virtual environment..
   call %CD%\%venv%\Scripts\activate.bat
-  echo done. installing dependencies...
+  echo done. installing dependencies..
   python -m pip install -r requirements.txt
 )
 
-echo running the bot...
+echo starting the spider..
 python main.py
 
 @pause
